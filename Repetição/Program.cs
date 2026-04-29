72543c490ce9e4237f3b3931fcdf704401b0b7df
KauaRocha01 <kauar6490@gmail.com>
2026-04-29 17:22:56 -0300

    Exercicios C#

---
+++ Repetição/Program.cs
@@ -0,0 +1,198 @@
+﻿namespace Repetição;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       // Console.WriteLine("Hello, World!");
+
+    /*
+        for -> usado para quando você não sabe quantas vezes vai repetir
+
+        while -> usado para quando você não sabe quntas vezes vai repetir a repetição 
+        acontece enquanto uma condição for verdadeira
+
+        do/while -> é parecido com while, mas a diferença é que
+        mesmo que a condição seja falsa. ele executa pelo menos
+        uma vez: "faz primeiro e depois pergunta"
+
+        foreach
+        -> usado para percorrer listas
+    */
+
+    //  for(int i = 1; i <= 5; i++)
+    //  {
+    //      Console.WriteLine(i);
+    //  }
+
+    // int soma = 0;
+    // for (int i = 1; i <= 10; i++)
+    //  {
+    //      soma = soma + i;
+    //      Console.WriteLine("Soma atual: " + soma);
+    //  }
+
+    // Console.WriteLine("Soma final: " + soma);
+
+    // int conta = 0;
+
+    // while(conta <= 5)
+    // {
+    //     Console.WriteLine(conta);
+    //     // contador = contador + 1
+    //     conta++;
+    // }
+
+    //     Console.WriteLine(conta);
+
+    // int numero1 = 5;
+    // int resultado2 = numero1++;
+
+    // Console.WriteLine(numero1);
+    // Console.WriteLine(resultado2);
+
+    // int numero = 0;
+
+    // while(numero <= 0)
+    // {
+    //     Console.WriteLine("Digite um número maior que zero: " );
+    //     numero = int.Parse(Console.ReadLine());
+    // }
+
+    // Console.WriteLine("Número válido: " + numero);
+
+    // int numero = 0;
+
+    // do 
+    // {
+    //     Console.WriteLine("Digite um número maior que zero: ");
+    //     numero = int.Parse(Console.ReadLine());
+    // }
+    // while(numero <= 0);
+
+    // Console.WriteLine("Número valido: " + numero);
+    
+    // int opcao;
+
+    // do 
+    // {
+    //     Console.WriteLine("1 = Continuar");
+    //     Console.WriteLine("2 - Sair");
+    //     opcao = int.Parse(Console.ReadLine());
+    // } while (opcao !=2 );
+
+    // Console.WriteLine("Programa encerrado");
+
+    //  string[] nomes = new string[] {"Ana", "Carlos", "Maria"};
+
+    //  foreach(string nome in nomes)
+    //  {
+    //      Console.WriteLine(nome);
+    //  }
+
+    // int[] numeros = { 2, 4, 6, 8};
+    // int soma = 0;
+
+    // foreach(int numero in numeros)
+    // {
+    //     soma += numero;
+    // }
+
+    // Console.WriteLine("Soma: " + soma);
+
+    //Contagem crescente (for)
+    //  for(int i = 1; i <= 10; i++)
+    //  {
+    //      Console.WriteLine(i);
+    //  }
+
+    //Contagem regressiva (for)
+    // for(int i = 10; i >= 0; i--)
+    // {
+    //     Console.WriteLine(i);
+    // }
+
+    //Tabuada simples (for)
+
+    // Console.Write("Digite um número: ");
+    // int numero = int.Parse(Console.ReadLine());
+    // for (int i = 1; i <= 10; i++)
+    // {
+    // Console.WriteLine($"{numero} x {i} = {numero * i}");
+    // }
+      
+    
+    //Repetição com while
+    
+    // int numero = 1;
+
+    // while(numero <= 5)
+    // {
+    // Console.WriteLine(numero);
+    // numero++;
+    // }
+
+    //Soma até 5 (for)
+    
+    // int soma = 0;
+
+    // for (int i = 1; i <= 5; i++)
+    // {
+    // soma = soma + i;
+    // }
+
+    // Console.WriteLine("Soma total: " + soma);
+
+
+    //Soma de números digitados (for)
+
+    // int soma = 0;
+
+    // for (int i = 1; i <= 5; i++)
+    // {
+    // Console.Write("Digite um número: ");
+    // int resultado = int.Parse(Console.ReadLine());
+
+    // soma += resultado;
+    // }
+
+    // Console.WriteLine("Soma total: " + soma);
+
+    //Contar pares (for)
+
+    // for (int i = 1; i <= 20; i++)
+    // {
+    // if (i % 2 == 0)
+    // {
+    //     Console.WriteLine(i);
+    // }
+    // }   
+
+    //Validação de senha (while)
+
+    // string senha;
+    // do
+    // {
+    // Console.Write("\n Digite a senha: ");
+    
+    // senha = Console.ReadLine();
+    
+    // } while (senha != "1234");
+    
+    // Console.WriteLine("Acesso permitido");
+
+
+
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
